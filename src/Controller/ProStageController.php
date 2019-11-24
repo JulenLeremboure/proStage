eb2dbdd7a725b0d8f5d5f633a312a80af65f4c12
LEREMBOURE Julen <jleremboure@iparla.iutbayonne.univ-pau.fr>
2019-11-24 18:10:18 +0100

    Test de màj du projet pour voir si ça marche

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProStageController extends AbstractController
 {
@@ -16,7 +17,33 @@ class ProStageController extends AbstractController
             'controller_name' => 'ProStageController',
         ]);
     }
-	
-	
+    
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+	public function msgBienvenue(){
+        return $this->render('pro_stage/entreprises.html.twig');
+    }
+
+    /**
+     * @Route("/pro/stage/entreprises", name="entreprises")
+     */
+    public function msgAccueilEntreprises(){
+
+    }
+
+    /**
+     * @Route("/pro/stage/formations", name="formations")
+     */
+    public function msgAccueilFormations(){
+
+    }
+
+    /**
+     * @Route("/pro/stage/stages/{id}", name="stageWithID")
+     */
+    public function msgAccueilDescriptifStage(int $id){
+
+    }
 
 }
