ab9f21be6464efea983821d62f11deb9ec741119
LEREMBOURE Julen <jleremboure@iparla.iutbayonne.univ-pau.fr>
2019-12-16 16:10:25 +0100

    Enfin une avancée dans le projet. Redéfinition de la strcture de la page d'accueil avec des block twig et création des liens vers des pages dans une liste

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,41 +9,34 @@ use Symfony\Component\HttpFoundation\Response;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="pro_stage")
      */
-    public function index()
+    public function afficherAccueil()
     {
         return $this->render('pro_stage/index.html.twig', [
             'controller_name' => 'ProStageController',
         ]);
     }
-    
+
     /**
      * @Route("/entreprises", name="entreprises")
      */
-	public function msgBienvenue(){
+    public function afficherEntreprises(){
         return $this->render('pro_stage/entreprises.html.twig');
     }
 
     /**
-     * @Route("/pro/stage/entreprises", name="entreprises")
+     * @Route("/formations", name="formations")
      */
-    public function msgAccueilEntreprises(){
-
+    public function afficherFormations(){
+        return $this->render('pro_stage/formations.html.twig');
     }
 
     /**
-     * @Route("/pro/stage/formations", name="formations")
+     * @Route("/stages/{id}", name="stage")
      */
-    public function msgAccueilFormations(){
-
-    }
-
-    /**
-     * @Route("/pro/stage/stages/{id}", name="stageWithID")
-     */
-    public function msgAccueilDescriptifStage(int $id){
-
+    public function afficherStage(int $id){
+        return $this->render('pro_stage/stage.html.twig',['id'=>$id,]);
     }
 
 }
